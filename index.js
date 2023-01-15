d435bf6e2bffa1313f9dcf5d4686802e2cfdef45
Daniel <danielgruttercpt@gmail.com>
2023-01-15 19:30:14 +0200

    updating map tiles

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-
 function preload() {
-  roadHorizontal = loadImage("./assets/road-horizontal.png");
-  roadVertical =loadImage('./assets/road-vertical.png')
-  roadEdgeDown = loadImage("./assets/road-edge-down.png");
-  roadEdgeUp = loadImage("./assets/road-edge-up.png");
-  roadEdgeRight = loadImage("./assets/road-edge-right.png");
-  roadEdgeLeft = loadImage("./assets/road-edge-left.png");
+  roadHorizontal = loadImage('./assets/road-horizontal.png');
+  roadVertical = loadImage('./assets/road-vertical.png');
+  roadEdgeDown = loadImage('./assets/road-edge-down.png');
+  roadEdgeUp = loadImage('./assets/road-edge-up.png');
+  roadEdgeRight = loadImage('./assets/road-edge-right.png');
+  roadEdgeLeft = loadImage('./assets/road-edge-left.png');
+  roadTransferLeftUp = loadImage('./assets/road-transfer-left-up.png');
+  roadTransferLeftDown = loadImage('./assets/road-transfer-left-down.png');
+  roadTransferRightDown = loadImage("./assets/road-transfer-right-down.png");
+  roadHorizontalDown = loadImage("./assets/road-horizontal-down.jpg");
 }
 function setup() {
-  let tiles = [
+  let mapTiles = [
     // 2d array of image data
     [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
     [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
     [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
     [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
     [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+    [null,null,null,null,null,roadTransferLeftDown,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadTransferRightDown,null,null,null,null,null,null,null],
+    [null,null,null,null,null,roadVertical,null,null,null,null,null,null,roadVertical,null,null,null,null,null,null,null],
+    [null,null,null,null,null,roadVertical,null,null,null,null,null,null,roadVertical,null,null,null,null,null,null,null],
+    [null,null,roadEdgeLeft,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontalDown,roadHorizontal,roadHorizontal,roadTransferRightDown,null,null,null,null],
+    [null,null,null,null,null,null,null,null,null,null,null,null,roadVertical,null,null,roadVertical,null,null,null,null],
+    [null,null,null,null,null,null,null,null,null,null,null,null,roadVertical,null,null,roadVertical,null,null,null,null],
+    [null,null,null,null,null,null,null,null,null,null,null,null,roadVertical,null,null,roadVertical,null,null,null,null],
+    [null,null,null,null,null,null,null,null,null,null,null,null,roadTransferLeftUp,roadHorizontal,roadHorizontal,roadTransferRightDown,null,null,null,null],
     [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
     [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
     [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
-    [null,null,null,null,null,roadEdgeUp,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadEdgeRight,null,null,null,null,null],
-    [null,null,null,null,null,roadVertical,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
-    [null,null,null,null,null,roadVertical,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
-    [null,null,null,null,null,roadVertical,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
-    [null,null,null,null,null,roadVertical,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
-    [null,null,null,null,null,roadEdgeLeft,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadHorizontal,roadEdgeRight,null,null,null,null,null],
     [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
     [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
     [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
     [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
-  ];
+    [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+
+  ]
+  let gameTiles = [[]];
   gameCanvas = createCanvas(WIDTH, HEIGHT);
-  gameCanvas.parent("canvas-container");
+  gameCanvas.parent('canvas-container');
   background(25, 25, 25);
   //   noStroke();
   // let button = createImage(100,100);
   // button.position(300,300);
   // button.mousePressed(TestClicked)
-  drawMap(tiles);
+  drawMap(mapTiles);
+  drawGame(gameTiles);
 }
 function draw() {}
-function TestClicked() {
-  alert("this is a test");
+
+function windowResized() {
+  resizeCanvas(windowWidth - (20 / 100) * windowWidth, windowHeight);
 }
--- objects/grid.js
+++ objects/grid.js
@@ -4,16 +4,16 @@ function drawMap(mapTiles) {
       if(mapTiles[x][y] !== null){
         // rect(y*TILEWIDTH,x*TILEHEIGHT,TILEWIDTH,TILEHEIGHT);
         image(mapTiles[x][y],y*TILEWIDTH,x*TILEHEIGHT,TILEWIDTH,TILEHEIGHT)
-      } 
+      }
     }
   }
 }
 function drawGame(gameTiles) {
-  for (let x = 0; x < mapTiles.length; x++) {
-    for (let y = 0; y < mapTiles[x].length; y++) {
-      if(mapTiles[x][y] !== null){
+  for (let x = 0; x < gameTiles.length; x++) {
+    for (let y = 0; y < gameTiles[x].length; y++) {
+      if(gameTiles[x][y] !== null){
         // rect(y*TILEWIDTH,x*TILEHEIGHT,TILEWIDTH,TILEHEIGHT);
-        image(mapTiles[x][y],y*TILEWIDTH,x*TILEHEIGHT,TILEWIDTH,TILEHEIGHT)
+        rect(y*TILEWIDTH,x*TILEHEIGHT,TILEWIDTH/2,TILEHEIGHT/2)
       } 
     }
   }
